02be4b892ac9590385835906d7d5609b97302203
Luiz Henrique Martinelli <83023291+luiz-martinelli@users.noreply.github.com>
2022-03-25 09:46:43 -0300

    imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'core/inject/inject.dart';
+import 'layers/presentation/ui/pages/home/home_page.dart';
+
 void main() {
+  Inject.init();
   runApp(const MyApp());
 }
 
@@ -10,11 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
