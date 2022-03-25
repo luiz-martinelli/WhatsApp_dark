e6a652ece0435d447a6c05c96606af0fc7c7370e
Luiz Henrique Martinelli <83023291+luiz-martinelli@users.noreply.github.com>
2022-03-25 10:13:15 -0300

    image

--- lib/layers/data/datasources/local/get_messages_local_datasource_imp.dart
+++ lib/layers/data/datasources/local/get_messages_local_datasource_imp.dart
@@ -6,61 +6,61 @@ class GetMessagesLocalDatasourceImpl implements GetMessagesDatasource {
   var json = [
     {
       "usuario": "Amor",
-      "usuarioImagem": "assets/perfil1.jpg",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Compra pão",
       "horario": "16:22"
     },
     {
       "usuario": "Mãe",
-      "usuarioImagem": "assets/perfi2.jpg",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Olá, tudo bem?",
       "horario": "16:21"
     },
     {
       "usuario": "Pedal Maravilha",
-      "usuarioImagem": "assets/perfil3.png",
+      "usuarioImagem": "assets/default.png",
       "ultimaMensagem": "Maci: Bora pedalar",
       "horario": "16:20"
     },
     {
       "usuario": "Usuário 123",
-      "usuarioImagem": "assets/perfil4.jpg",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Olá, tudo bem?",
       "horario": "16:19"
     },
     {
       "usuario": "Empresa",
-      "usuarioImagem": "assets/perfil5.jpg",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Rodrigo: Parabéns!!",
       "horario": "16:18"
     },
     {
       "usuario": "Usuário XYZ",
-      "usuarioImagem": "assets/perfil6.jpg",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "tudo bem",
       "horario": "16:17"
     },
     {
       "usuario": "Grupo Amigos",
-      "usuarioImagem": "assets/perfil.jpg",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Mozer: Bora fazer uma carne",
       "horario": "16:16"
     },
     {
       "usuario": "+55 49 9102-0304",
-      "usuarioImagem": "assets/default.png",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Tem promoção",
       "horario": "16:15"
     },
     {
       "usuario": "+55 49 9102-0305",
-      "usuarioImagem": "assets/default.png",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Obrigado pelo retorno",
       "horario": "16:14"
     },
     {
       "usuario": "+55 49 9102-0306",
-      "usuarioImagem": "assets/default.png",
+      "usuarioImagem": "assets/default.jpg",
       "ultimaMensagem": "Seu pedido saiu para entrega",
       "horario": "16:13"
     },
